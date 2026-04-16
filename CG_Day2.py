b12258a24d300e6720a8c0161c14e7a724be2d85
Anish Sankar <151515636+srianish@users.noreply.github.com>
2026-04-16 10:44:34 +0530

    Today we discussed about -variables -keywords
    -comments 
    -boolen type

---
+++ CG_Day2.py
@@ -0,0 +1,106 @@
+'''
+Variables
+--------
+->good way to define a variable
+A=90
+print(A)
+b=80
+print(b)
+_c=70-
+print(_c)
+
+->bad way to define variable
+9a=90
+print(9a)
+
+->we should not use space
+ num=9
+
+ note:We are going to use meaningfull words or names for defining variables.
+-------------------------------------------------------------------
+
+Keywords
+--------
+->This keywords not gonna use a variable like if,ifelse,while....
+
+a,b,c=1,2,3
+print(a+b)
+print(f"\n{a} \n{b} \n{c}")
+
+print(a)
+print(b)
+print(c)
+print(a,b,c)
+
+------------------------------------------------------
+
+Tokens
+------
+->Nothing but a small program or code in python to complete the task
+a, b, c=1,2,3
+print(a+b)
+----------------------------------------------------
+
+Comments
+---------
+Comments are 2 types Single line comments and Multiline Comments
+1.Single line :
+Used to explain about that particular line in the code, for this we can use '#' symbol
+a, b, c = 1, 2, 3 #here in the line, i have assign 3 variable and values
+print(a+b)
+2.Multiline :
+To comment more than single line we can use multiline commenting
+""" """ or ''' '''
+by using these symboles the cursor wont consider
+-------------------------------------------------------------------------
+
+Boolen Type
+-----------
+->To find True or False
+
+a, b = 9, 10
+print(a == b)
+print(a != b)
+
+num = 7
+if num%2 == 0:
+    print(f"{num} is even number")
+else:
+    print(f"{num} is odd number")
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
